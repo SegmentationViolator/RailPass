4e19c1587f3f26f04f591a00758eb8cfa0051f70
Segmentation Violator <segmentationviolator@proton.me>
2025-10-06 07:09:57 +0530

    stuff

--- src/main/java/com/railpass/web/GlobalControllerAdvice.java
+++ src/main/java/com/railpass/web/GlobalControllerAdvice.java
@@ -1,7 +1,11 @@
 package com.railpass.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,4 +17,9 @@ public class GlobalControllerAdvice {
         response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 		return "forward:/static/index.html";
 	}
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<String> missingHeader(HttpServletResponse response) {
+        return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+    }
 }
